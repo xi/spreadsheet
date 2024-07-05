864914d7aa02b1ac360dfc58fa39fc9c0eacf39c
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-07-05 20:27:45 +0200

    boon UI

--- sheet/__main__.py
+++ sheet/__main__.py
@@ -1,9 +1,127 @@
 import sys
 
+import boon
+
 from .csv import load_csv
-from .term import render
+from .expression import x2col
+from .term import align_center
+from .term import align_left
+from .term import align_right
+from .term import red
+from .term import invert
 
-with open(sys.argv[1]) as fh:
-    sheet = load_csv(fh)
 
-print(render(sheet, 80, 40, (0, 0), 10))
+def to_cell(value: float|int|str|None|Exception, width: int) -> str:
+    if isinstance(value, float):
+        return align_right(str(value), width)
+    elif isinstance(value, int):
+        return align_right(str(value), width)
+    elif isinstance(value, str):
+        return align_left(value, width)
+    elif value is None:
+        return ' ' * width
+    elif isinstance(value, Exception):
+        return red(align_left(repr(value), width))
+
+
+class App(boon.App):
+    def __init__(self):
+        super().__init__()
+        with open(sys.argv[1]) as fh:
+            self.sheet = load_csv(fh)
+        self.x0 = 0
+        self.y0 = 0
+        self.cursor_x = 0
+        self.cursor_y = 0
+        self.widths = {}
+
+    def scroll_into_view(self, rows, cols):
+        if self.cursor_y < self.y0:
+            self.y0 = self.cursor_y
+        elif self.cursor_y > self.y0 + rows - 3:
+            self.y0 = self.cursor_y - rows + 3
+
+        if self.cursor_x < self.x0:
+            self.x0 = self.cursor_x
+        else:
+            widths = [self.get_width(x) for x in range(self.x0, self.cursor_x + 1)]
+            offset = 0
+            while sum(widths[offset:]) > cols:
+                offset += 1
+            self.x0 += offset
+
+    def render(self, rows, cols):
+        self.scroll_into_view(rows, cols)
+
+        lines = []
+
+        lines.append(' ' * 4)
+        x = self.x0
+        while len(lines[-1]) < cols:
+            row_head = align_center(x2col(x), self.get_width(x))
+            if x == self.cursor_x:
+                row_head = invert(row_head)
+            lines[-1] += row_head
+            x += 1
+
+        for dy in range(rows - 2):
+            y = self.y0 + dy
+            # FIXME: col_head vanishes
+            col_head = align_right(str(y + 1), 4)
+            if y == self.cursor_y:
+                col_head = invert(col_head)
+            lines.append(col_head)
+
+            x = self.x0
+            # FIXME: do not count ANSI codes for length
+            # FIXME; lines are longer than they should be (we stop once we are >=)
+            while len(lines[-1]) < cols:
+                value = self.sheet.get_value((x, y))
+                cell = to_cell(value, self.get_width(x))
+                if x == self.cursor_x and y == self.cursor_y:
+                    cell = invert(cell)
+                lines[-1] += cell
+                x += 1
+
+        lines.append(self.sheet.get_raw((self.cursor_x, self.cursor_y)))
+
+        return lines
+
+    def get_width(self, x):
+        return self.widths.get(x, 10)
+
+    def set_width(self, x, value):
+        self.widths[x] = max(value, 3)
+
+    def change_width(self, x, d):
+        old = self.get_width(x)
+        self.set_width(x, old + d)
+
+    def on_key(self, key):
+        if key == 'q':
+            self.running = False
+        elif key == boon.KEY_DOWN:
+            self.cursor_y += 1
+        elif key == boon.KEY_UP:
+            self.cursor_y = max(self.cursor_y - 1, 0)
+        elif key == boon.KEY_NPAGE:
+            self.cursor_y += 20  # TODO: relativ to rows
+        elif key == boon.KEY_PPAGE:
+            self.cursor_y = max(self.cursor_y - 20, 0)
+        elif key == boon.KEY_RIGHT:
+            self.cursor_x += 1
+        elif key == boon.KEY_LEFT:
+            self.cursor_x = max(self.cursor_x - 1, 0)
+        elif key == '>':
+            self.change_width(self.cursor_x, 1)
+        elif key == '<':
+            self.change_width(self.cursor_x, -1)
+        elif key == '=':
+            pass
+            # self.set_width(self.cursor_x, max()  # TODO auto width
+        elif key == '\n':
+            pass  # TODO: edit
+
+
+app = App()
+app.run()
--- sheet/term.py
+++ sheet/term.py
@@ -1,6 +1,3 @@
-from .expression import x2col
-
-
 def align_right(s, width):
     if len(s) > width:
         s = '###'
@@ -26,36 +23,3 @@ def red(s):
 
 def invert(s):
     return f'\033[7m{s}\033[0m'
-
-
-def to_cell(value: float|int|str|None|Exception, width: int) -> str:
-    if isinstance(value, float):
-        return align_right(str(value), width)
-    elif isinstance(value, int):
-        return align_right(str(value), width)
-    elif isinstance(value, str):
-        return align_left(value, width)
-    elif value is None:
-        return ' ' * width
-    elif isinstance(value, Exception):
-        return red(align_left(repr(value), width))
-
-
-def render(sheet, width, height, cell_offset, cell_width):
-    x0, y0 = cell_offset
-    rows = []
-    w = width // cell_width
-    rows.append([
-        ' ' * cell_width,
-    ] + [
-        align_center(x2col(x0 + dx), cell_width)
-        for dx in range(w - 1)
-    ])
-    for dy in range(height - 1):
-        rows.append([
-            align_right(str(y0 + dy + 1), cell_width),
-        ] + [
-            to_cell(sheet.get_value((x0 + dx, y0 + dy)), cell_width)
-            for dx in range(w - 1)
-        ])
-    return '\n'.join([''.join(row) for row in rows])
