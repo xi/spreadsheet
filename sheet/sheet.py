9a1a335d2e4dadd371fee6e06f579f74145ba164
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-07-21 11:59:26 +0200

    fix: bar with when full

--- sheet/sheet.py
+++ sheet/sheet.py
@@ -11,9 +11,12 @@ class Bar:
 
     def render(self, width):
         value = max(0, min(1, self.value))
-        x = int(value * width * (len(BLOCKS) - 1))
+        x = round(value * width * (len(BLOCKS) - 1))
         a, b = divmod(x, len(BLOCKS) - 1)
-        return a * BLOCKS[-1] + BLOCKS[b] + (width - a - 1) * BLOCKS[0]
+        if a == width:
+            return a * BLOCKS[-1]
+        else:
+            return a * BLOCKS[-1] + BLOCKS[b] + (width - a - 1) * BLOCKS[0]
 
 
 def iter_range(cell1, cell2):
